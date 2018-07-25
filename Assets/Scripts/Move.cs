0f03f2900fbc512377e5b2030f019830e07fac04
Daniel Unapanta <dunapanta2@gmail.com>
2018-07-25 01:35:28 -0500

    Cambio para movimiento con motor fisico para el giro de la esfera aplicando fuerzas con el teclado y la gravedad

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,30 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Move : MonoBehaviour {
 
 	//Variable global para definir la velocidad
-	public float speed;
+	public float forceValue;
+	//Variable para la fuerza utilizando Rigidbody
+	private Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () {
-	
+		//Para optimizar el rendimiento se inicializa en Start
+		// y se almacena la componente en la variable creada
+		//se evita la sobrecarga de llamar getcomponente en FixedUpdate
+		//GetComponente se puede acceder a componentes de gameobject
+		rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	//transform tiene la posiscion, rotacion y escala
-	//para cambiar la posicion se utiliza el metodo translate
-	//Para controlar movimiento se gestiona con Input (teclas,
-		// acelerometro gamepad, etc)
-	//GetAxis("Horizontal") puede mover con las teclas o en 
-		//gamepad izquierda o derecha valores (-1 o 1)
-		//Time.deltaTime para poder especificar en metros
+	}
 
-	transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 
-		0, 
-		Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	// Se ejecuta por defecto 50 veces por segundo
+	//Codigo que se sincroniza con el motor fisico 
+	// En este caso se empuja se aplica una fuerza constante 
+	//a la esfera con las teclas del cursor
+	void FixedUpdate () {
+		//Metodo AddForce añade una fuerza a la componente Rigidbody del gameobject
+		//Genera fuerza de emuje para que ruede
+		//Vector3 es de tres componentes
+		rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"),
+			0,
+			Input.GetAxis("Vertical")) * forceValue);
+
 
 	}
 }
